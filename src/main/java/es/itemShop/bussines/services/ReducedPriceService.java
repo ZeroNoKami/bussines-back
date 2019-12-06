c496e650f45d79b1772dc25869516536113d690d
Zero <zeronokami13@gmail.com>
2019-12-06 18:40:28 +0100

    Eliminacion de warings

--- src/main/java/es/itemShop/bussines/services/ReducedPriceService.java
+++ src/main/java/es/itemShop/bussines/services/ReducedPriceService.java
@@ -1,13 +1,8 @@
 package es.itemShop.bussines.services;
 
 import java.util.List;
-import java.util.Optional;
-
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import es.itemShop.bussines.model.ReducedPriceDao;
